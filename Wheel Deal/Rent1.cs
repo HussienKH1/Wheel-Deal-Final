e3775541ab0d225590b8bac814477b86b4597a5e
bissanalmiari <bissanalmiari@gmail.com>
2024-03-27 14:49:33 +0200

    done

--- Wheel Deal/Rent1.cs
+++ Wheel Deal/Rent1.cs
@@ -13,6 +13,7 @@ namespace Wheel_Deal
 {
     public partial class Rent1 : Form
     {
+        SqlConnection con = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["myDB"].ToString());
         public Rent1()
         {
             InitializeComponent();
@@ -26,6 +27,27 @@ namespace Wheel_Deal
         private void guna2GradientButton1_Click(object sender, EventArgs e)
         {
 
+            try
+            {
+                if (con.State != ConnectionState.Open)
+                    con.Open();
+                SqlCommand cmd = new SqlCommand("Rent", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@SSN", Int64.Parse(txt_SSN.Text));
+                cmd.Parameters.AddWithValue("@CRID", Int64.Parse(txt_CID.Text));
+                cmd.Parameters.AddWithValue("@RentID", Int64.Parse(txt_RID.Text));
+                cmd.Parameters.AddWithValue("@Startdate", picker_SD.Value);
+                cmd.Parameters.AddWithValue("@Enddate", picker_ED.Text);
+                cmd.Parameters.AddWithValue("@Price", txt_price.Text);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("rented");
+                if (con.State == ConnectionState.Open)
+                    con.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("error!!!");
+            }
         }
     }
 }
